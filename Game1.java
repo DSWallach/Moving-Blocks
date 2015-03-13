ee5e5850a5f00646cb8978d0e3b98d5d76a1966b
David Wallach <dawallach@vassar.edu>
2015-03-13 00:21:37 -0400

    Tests added, unnecessary Classes and code removed

--- Game1.java
+++ Game1.java
@@ -1,10 +1,8 @@
 import java.awt.*;
 import java.util.*;
-import java.lang.*;
 import tester.*;
 import javalib.funworld.*;
 import javalib.colors.*;
-import javalib.worldcanvas.*;
 import javalib.worldimages.*;
 
 class Platform{
@@ -39,17 +37,6 @@ class Platform{
 	}
     }
 }
-class Ground{
-    
-    int num;
-    
-    Ground (int num){
-	this.num = num;
-    }
-    public WorldImage groundImage(){
-	return new RectangleImage(new Posn(210, 590), 420, 20, new Black());
-    }
-}
 class Goal {
     
     int num;
@@ -75,35 +62,30 @@ class Goal {
 class PBlock{
     
     Posn center;
-    int height;
-    int width;
     IColor col;
 
-    PBlock(Posn center, int width, int height, IColor col){
+    PBlock(Posn center, IColor col){
 	this.center = center;
-	this.height = height;
-	this.width = width;
 	this.col = col;
     }
     public WorldImage blockImage(){
 	return new RectangleImage(this.center,
-				  this.width,
-				  this.height,
+				  20, 20,
 				  this.col);
     }
     public PBlock moveBlock(String ke){
-	if (ke.equals("right") && (420 > this.center.x + (this.width / 2))){
+	if (ke.equals("right") && (420 > this.center.x + 10)){
 	    return new PBlock(new Posn(this.center.x + 20, this.center.y),
-			      this.width, this.height, this.col);
-	} else if (ke.equals("left") && (0 < this.center.x - (this.width / 2))){
+			      this.col);
+	} else if (ke.equals("left") && (0 < this.center.x - 10)){
 	    return new PBlock(new Posn(this.center.x - 20, this.center.y),
-			      this.width, this.height, this.col);
-	} else if (ke.equals("up") && (0 < this.center.y - (this.height / 2))){
+			      this.col);
+	} else if (ke.equals("up") && (0 < this.center.y - 10)){
 	    return new PBlock(new Posn(this.center.x, this.center.y - 10),
-			      this.width, this.height, this.col);
-	} else if (ke.equals("down") && (600 > this.center.y + (this.width / 2))){
+			      this.col);
+	} else if (ke.equals("down")){
 	    return new PBlock(new Posn(this.center.x, this.center.y + 20),
-			      this.width, this.height, this.col);
+			      this.col);
 	} else {   
 	    return this;
 	}
@@ -115,39 +97,39 @@ class Game1 extends World {
     static int height = 600;
     int score;
     PBlock block;
-    Ground ground;
     Goal goal;
     Platform P1;
     Platform P2;
     Platform P3;
+    WorldImage gameArena = new RectangleImage(new Posn((this.width / 2),
+						       (this.height / 2)),
+					      this.width,
+					      this.height,
+					      new Blue());
     
-    public Game1 (int score, PBlock block, Ground ground, Goal goal,
+    public Game1 (int score, PBlock block, Goal goal,
 		  Platform P1, Platform P2, Platform P3){
 	super();
 	this.score = score;
 	this.block = block;
-	this.ground = ground;
 	this.goal = goal;
 	this.P1 = P1;
 	this.P2 = P2;
 	this.P3 = P3;
     }
-    public WorldImage gameArena = new RectangleImage(new Posn((this.width / 2),
-							      (this.height / 2)),
-						     this.width,this.height, new Blue());
     public World onKeyEvent(String ke){
 	if (ke.equals("x")){
+	    
 	    return this.endOfWorld("Aidos");
 	} else {
 	    return new Game1 (this.score, this.block.moveBlock(ke),
-			      this.ground, this.goal, this.P1, this.P2, this.P3);
+			      this.goal, this.P1, this.P2, this.P3);
 	}
     }
     public World onTick(){
 	if (this.block.center.x == this.goal.num && (570 < this.block.center.y)){
 	    return new Game1 ((this.score + 100),
-			      new PBlock(new Posn(210,10), 20, 20, new Red()),
-			      this.ground,
+			      new PBlock(new Posn(210,10), new Red()),
 			      this.goal.newGoal(),
 			      new Platform(this.P1.center,
 					   (this.P1.speed+3),
@@ -163,17 +145,15 @@ class Game1 extends World {
 					   this.P3.dir).movePlatform());
 	} else if(570 < this.block.center.y){
 	    return new Game1 ((this.score - 50),
-			      new PBlock(new Posn(210,10), 20, 20, new Red()),
-			      this.ground,
+			      new PBlock(new Posn(210,10), new Red()),
 			      this.goal,
 			      this.P1.movePlatform(),
 			      this.P2.movePlatform(),
 			      this.P3.movePlatform());
 	} else if ((Math.abs(this.block.center.y-this.P1.center.y)<=20) &&
-		   ((this.P1.width+this.block.width) /2) >= Math.abs(this.block.center.x - this.P1.center.x)){
+		   ((this.P1.width+20) /2) >= Math.abs(this.block.center.x - this.P1.center.x)){
 	    return new Game1 ((this.score - 25),
-			      new PBlock(new Posn(210,10), 20, 20, new Red()),
-			      this.ground,
+			      new PBlock(new Posn(210,10), new Red()),
 			      this.goal,
 			      new Platform(this.P1.center,
 					   this.P1.speed,
@@ -182,10 +162,9 @@ class Game1 extends World {
 			      this.P2.movePlatform(),
 			      this.P3.movePlatform());
 	} else if((Math.abs(this.block.center.y-this.P2.center.y)<=20) &&
-		  ((this.P2.width+this.block.width) /2) >= Math.abs(this.block.center.x - this.P2.center.x)){
+		  ((this.P2.width+20) /2) >= Math.abs(this.block.center.x - this.P2.center.x)){
 	    return new Game1 ((this.score - 25),
-			      new PBlock(new Posn(210,10), 20, 20, new Red()),
-			      this.ground,
+			      new PBlock(new Posn(210,10), new Red()),
 			      this.goal,
 			      this.P1.movePlatform(),      
 			      new Platform(this.P2.center,
@@ -194,10 +173,9 @@ class Game1 extends World {
 					   this.P2.dir),
 			      this.P3.movePlatform());
 	} else if((Math.abs(this.block.center.y-this.P3.center.y)<=20) &&
-		  ((this.P3.width+this.block.width) /2) >= Math.abs(this.block.center.x - this.P3.center.x)){
+		  ((this.P3.width+20) /2) >= Math.abs(this.block.center.x - this.P3.center.x)){
 	    return new Game1 ((this.score - 25),
-			      new PBlock(new Posn(210,10), 20, 20, new Red()),
-			      this.ground,
+			      new PBlock(new Posn(210,10), new Red()),
 			      this.goal,
 			      this.P1.movePlatform(),
 			      this.P2.movePlatform(),
@@ -208,7 +186,6 @@ class Game1 extends World {
 	} else {
 	    return new Game1 (this.score,
 			      this.block.moveBlock("down"),
-			      this.ground,
 			      this.goal,
 			      this.P1.movePlatform(),
 			      this.P2.movePlatform(),
@@ -217,20 +194,20 @@ class Game1 extends World {
     }
     public WorldImage makeImage(){
 	return new OverlayImages(
-				 new OverlayImages(
-						   new OverlayImages(
-								     new OverlayImages(
-										       new OverlayImages(
-													 new OverlayImages(
-															   new OverlayImages(
-																	     this.gameArena,
-																	     this.ground.groundImage()),
-															   this.goal.goalImage()),
-													 this.block.blockImage()),
-										       this.P1.platformImage()),
-								     this.P2.platformImage()),
-						   this.P3.platformImage()),
-				 new TextImage(new Posn(300, 20), "Your score is " + this.score,Color.red)); 
+		   new OverlayImages(
+		       new OverlayImages(
+		           new OverlayImages(
+			       new OverlayImages(
+				   new OverlayImages(
+				       new OverlayImages(
+				           this.gameArena,
+					   new RectangleImage(new Posn(210,590), 420, 20, new Black())),
+				         this.goal.goalImage()),
+				    this.block.blockImage()),
+			       this.P1.platformImage()),
+			   this.P2.platformImage()),
+		       this.P3.platformImage()),
+		   new TextImage(new Posn(300, 20), "Your score is " + this.score,Color.red)); 
     }
     public WorldEnd worldEnds(){
 	if (P1.width >= width || P2.width >= width || P3.width >= width){
@@ -246,8 +223,7 @@ class Game1 extends World {
     }
     public static void main(String args[]){
 	Game1 G = new Game1(0,
-			    new PBlock(new Posn(210, 10),20, 20,new Red()),
-			    new Ground(21),
+			    new PBlock(new Posn(210, 10), new Red()),
 			    new Goal(70),
 			    new Platform (new Posn (310, 150), 10, 150, "left"),
 			    new Platform (new Posn (110, 300), 10, 150, "right"),
@@ -256,7 +232,7 @@ class Game1 extends World {
     }
 }
 
-class GameExamples{
+class GameTest{
     
     // EXAMPLE PLATFORMS
     Platform platform1 = new Platform (new Posn (310, 150), 10, 150, "left");
@@ -273,69 +249,73 @@ class GameExamples{
     Platform rightEdge = new Platform (new Posn (345, 450), 10, 150, "right");
     Platform switchRight = new Platform (new Posn (85, 450), 10, 150, "right");
     Platform switchLeft = new Platform (new Posn (335, 450), 10, 150, "left");
-
-    // Standard Ground
-    Ground standardGround = new Ground(21);
     
-    // Standard Goal
+    // STANDARD GOAL
     Goal standardGoal = new Goal(70);
 
     // EXAMPLE PLAYER BLOCKS 
-    PBlock SBlock = new PBlock (new Posn(210, 10),20, 20,new Red());
-    PBlock upSBlock = new PBlock (new Posn(210, 10),20, 20,new Red());
-    PBlock downSBlock = new PBlock (new Posn(210, 30),20, 20,new Red());
-    PBlock leftSBlock = new PBlock (new Posn(190, 10),20, 20,new Red());
-    PBlock rightSBlock = new PBlock (new Posn(230, 10),20, 20,new Red());
-    PBlock p1DeathBlock = new PBlock (new Posn(310, 150),20, 20,new Red());
-    PBlock p2DeathBlock = new PBlock (new Posn(110, 300),20, 20,new Red());
-    PBlock p3DeathBlock = new PBlock (new Posn(210, 450),20, 20,new Red());
+    PBlock SBlock = new PBlock (new Posn(210, 10), new Red());
+    PBlock Goal = new PBlock (new Posn (70, 600), new Red());
+    PBlock noGoal = new PBlock (new Posn (50, 600), new Red());
+    PBlock upSBlock = new PBlock (new Posn(210, 10), new Red());
+    PBlock downSBlock = new PBlock (new Posn(210, 30), new Red());
+    PBlock leftSBlock = new PBlock (new Posn(190, 10), new Red());
+    PBlock rightSBlock = new PBlock (new Posn(230, 10), new Red());
+    PBlock p1DeathBlock = new PBlock (new Posn(310, 150), new Red());
+    PBlock p2DeathBlock = new PBlock (new Posn(110, 300), new Red());
+    PBlock p3DeathBlock = new PBlock (new Posn(210, 450), new Red());
 
     // EXAMPLE GAME WORLDS
     Game1 standardGame = new Game1(0,
 				   SBlock,
-				   standardGround,
 				   standardGoal,
 				   platform1,
 				   platform2,
 				   platform3);
+    Game1 goalGame = new Game1(0,
+			       Goal,
+			       standardGoal,
+			       platform1,
+			       platform2,
+			       platform3);
+    Game1 noGoalGame = new Game1(0,
+				 noGoal,
+				 standardGoal,
+				 platform1,
+				 platform2,
+				 platform3);
     Game1 P1Death = new Game1(0,
 			      p1DeathBlock,
-			      standardGround,
 			      standardGoal,
 			      platform1,
 			      platform2,
 			      platform3);
     Game1 P2Death = new Game1(0,
 			      p2DeathBlock,
-			      standardGround,
 			      standardGoal,
 			      platform1,
 			      platform2,
 			      platform3);
     Game1 P3Death = new Game1(0,
 			      p3DeathBlock,
-			      standardGround,
 			      standardGoal,
 			      platform1,
 			      platform2,
 			      platform3);
     Game1 P1End = new Game1(0,
 			    SBlock,
-			    standardGround,
 			    standardGoal,
 			    p1end,
 			    platform2,
 			    platform3);
     Game1 P2End = new Game1(0,
 			    SBlock,
-			    standardGround,
 			    standardGoal,
 			    platform1,
 			    p2end,
 			    platform3);
     Game1 P3End = new Game1(0,
 			    SBlock,
-			    standardGround,
 			    standardGoal,
 			    platform1,
 			    platform2,
@@ -364,15 +344,13 @@ class GameExamples{
 			  this.switchRight, "Test movePlatform - switch left to right" + "\n") &&
 	    t.checkExpect(this.rightEdge.movePlatform(),
 			  this.switchLeft, "Test movePlatform - swtich right to left" + "\n");
-	
     }
     // Testing Block/Platform Collision
     boolean testCollision(Tester t){
 	return
 	    t.checkExpect(this.P1Death.onTick(),
 			  new Game1 ((P1Death.score - 25),
-				     new PBlock(new Posn(210,10), 20, 20, new Red()),
-				     P1Death.ground,
+				     new PBlock(new Posn(210,10), new Red()),
 				     P1Death.goal,
 				     new Platform(P1Death.P1.center,
 						  P1Death.P1.speed,
@@ -383,8 +361,7 @@ class GameExamples{
 			  "Test Platform Collision - Platform 1") &&
 	    t.checkExpect(this.P2Death.onTick(),
 			  new Game1 ((P2Death.score - 25),
-				     new PBlock(new Posn(210,10), 20, 20, new Red()),
-				     P2Death.ground,
+				     new PBlock(new Posn(210,10), new Red()),
 				     P2Death.goal,
 				     P2Death.P1.movePlatform(),
 				     new Platform(P2Death.P2.center,
@@ -395,8 +372,7 @@ class GameExamples{
 			  "Test Platform Collision - Platform 2") &&
 	    t.checkExpect(this.P3Death.onTick(),
 			  new Game1 ((P3Death.score - 25),
-				     new PBlock(new Posn(210,10), 20, 20, new Red()),
-				     P3Death.ground,
+				     new PBlock(new Posn(210,10), new Red()),
 				     P3Death.goal,
 				     P3Death.P1.movePlatform(),
 				     P3Death.P2.movePlatform(),
@@ -434,11 +410,33 @@ class GameExamples{
 			  new WorldEnd(false, standardGame.makeImage()),
 			  "Test worldEnd - World shouldn't end" + "\n");
     }
-    
+    // Testing conditions for scoring goals
+    boolean testGoal(Tester t){
+	return
+	    t.checkExpect(this.goalGame.score,
+			  0,
+			  "Test goal functionality" + "\n") &&
+	    t.checkExpect(this.noGoalGame.onTick(),
+			  new Game1 (-50,
+				     SBlock,
+				     noGoalGame.goal,
+				     new Platform(noGoalGame.P1.center,
+						  (noGoalGame.P1.speed),
+						  noGoalGame.P1.width,
+						  noGoalGame.P1.dir).movePlatform(),
+				     new Platform(noGoalGame.P2.center,
+						  (noGoalGame.P2.speed),
+						  noGoalGame.P2.width,
+						  noGoalGame.P2.dir).movePlatform(),
+				     new Platform(noGoalGame.P3.center,
+						  (noGoalGame.P3.speed),
+						  noGoalGame.P3.width,
+						  noGoalGame.P3.dir).movePlatform()),
+			  "Test ground functionality" + "\n");
+    }
     public static void main(String[] argv){
-
 	// run the tests - showing only the failed test results
-	GameExamples be = new GameExamples();
+	GameTest be = new GameTest();
 	Tester.runReport(be, false, false);
     }
 }
